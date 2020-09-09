5a42e92330592f309b15e4090beea054d1c0e675
ganesh0479 <ganesh0479@gmail.com>
2020-09-10 00:10:55 +0530

    removed sample test case added to check code coverage

--- domain/src/main/java/com/go/tiny/business/helper/UserHelper.java
+++ domain/src/main/java/com/go/tiny/business/helper/UserHelper.java
@@ -1,12 +1,3 @@
 package com.go.tiny.business.helper;
 
-import com.go.tiny.business.model.User;
-
-public enum UserHelper {
-  USER_HELPER;
-
-  public User addUser(User user) {
-    User response = new User(user.emailId(),user.password());
-    return response;
-  }
-}
+public enum UserHelper {}
--- domain/src/test/java/com/go/tiny/business/UserHelperTest.java
+++ domain/src/test/java/com/go/tiny/business/UserHelperTest.java
@@ -1,18 +1,12 @@
 package com.go.tiny.business;
 
-import com.go.tiny.business.model.User;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
-import static com.go.tiny.business.helper.UserHelper.USER_HELPER;
-
 @RunWith(JUnitPlatform.class)
 public class UserHelperTest {
 
-    @Test
-    void shouldAddUser(){
-        User user = new User("EmailId","Password");
-        User responseUser= USER_HELPER.addUser(user);
-    }
+  @Test
+  void shouldAddUser() {}
 }
