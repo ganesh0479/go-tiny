a64c3613022481c1340668af51e890db97d08821
ganesh0479 <ganesh0479@gmail.com>
2020-09-10 00:20:00 +0530

    added methods without business logic for user domain

--- domain/src/main/java/com/go/tiny/business/domain/UserDomain.java
+++ domain/src/main/java/com/go/tiny/business/domain/UserDomain.java
@@ -1,11 +1,24 @@
 package com.go.tiny.business.domain;
 
+import com.go.tiny.business.model.User;
 import com.go.tiny.business.port.ObtainUser;
+
+import static com.go.tiny.business.helper.UserHelper.USER_HELPER;
 
 public class UserDomain {
   private ObtainUser obtainUser;
 
   public UserDomain(ObtainUser obtainUser) {
     this.obtainUser = obtainUser;
+  }
+
+  public void register(final User user) {
+    USER_HELPER.initialize(obtainUser);
+    USER_HELPER.register(user);
+  }
+
+  public Boolean signIn(final User user) {
+    USER_HELPER.initialize(obtainUser);
+    return USER_HELPER.signIn(user);
   }
 }
--- domain/src/main/java/com/go/tiny/business/helper/UserHelper.java
+++ domain/src/main/java/com/go/tiny/business/helper/UserHelper.java
@@ -1,3 +1,21 @@
 package com.go.tiny.business.helper;
 
-public enum UserHelper {}
+import com.go.tiny.business.model.User;
+import com.go.tiny.business.port.ObtainUser;
+
+import static java.lang.Boolean.FALSE;
+
+public enum UserHelper {
+  USER_HELPER;
+  private ObtainUser obtainUser;
+
+  public void initialize(ObtainUser obtainUser) {
+    this.obtainUser = obtainUser;
+  }
+
+  public void register(final User user) {}
+
+  public Boolean signIn(final User user) {
+    return FALSE;
+  }
+}
