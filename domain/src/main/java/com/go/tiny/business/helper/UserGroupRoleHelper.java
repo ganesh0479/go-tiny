60a9a579b9e0b578cf0df44895f887d181987acf
ganesh0479 <ganesh0479@gmail.com>
2020-09-10 14:15:08 +0530

    (user-group-role-domain): added business logic for user group role domain

--- domain/src/main/java/com/go/tiny/business/helper/UserGroupRoleHelper.java
+++ domain/src/main/java/com/go/tiny/business/helper/UserGroupRoleHelper.java
@@ -1,7 +1,11 @@
 package com.go.tiny.business.helper;
 
+import com.go.tiny.business.exception.GoTinyDomainException;
 import com.go.tiny.business.model.UserGroupRole;
 import com.go.tiny.business.port.ObtainUserGroupRole;
+
+import static com.go.tiny.business.exception.GoTinyDomainExceptionMessage.USER_GROUP_ROLE_RIGHT_SIDE_PORT_UNAVAILABLE;
+import static java.util.Objects.isNull;
 
 public enum UserGroupRoleHelper {
   USER_GROUP_ROLE_HELPER;
@@ -11,5 +15,14 @@ public enum UserGroupRoleHelper {
     this.obtainUserGroupRole = obtainUserGroupRole;
   }
 
-  public void updateUserGroupRole(UserGroupRole userGroupRole) {}
+  public void updateUserGroupRole(final UserGroupRole userGroupRole) {
+    if (isPortNotAvailable()) {
+      throw new GoTinyDomainException(USER_GROUP_ROLE_RIGHT_SIDE_PORT_UNAVAILABLE);
+    }
+    obtainUserGroupRole.updateUserGroupRole(userGroupRole);
+  }
+
+  private Boolean isPortNotAvailable() {
+    return isNull(obtainUserGroupRole);
+  }
 }
