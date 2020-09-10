2a2e42490d548848548967ce35368853011af83b
ganesh0479 <ganesh0479@gmail.com>
2020-09-10 13:20:02 +0530

    (group-domain): added business logic of group domain

--- domain/src/main/java/com/go/tiny/business/helper/GroupHelper.java
+++ domain/src/main/java/com/go/tiny/business/helper/GroupHelper.java
@@ -3,6 +3,8 @@ package com.go.tiny.business.helper;
 import com.go.tiny.business.model.Group;
 import com.go.tiny.business.port.ObtainGroup;
 
+import static java.util.Objects.isNull;
+
 public enum GroupHelper {
   GROUP_HELPER;
   private ObtainGroup obtainGroup;
@@ -11,9 +13,28 @@ public enum GroupHelper {
     this.obtainGroup = obtainGroup;
   }
 
-  public void create(final Group group) {}
+  public void create(final Group group) {
+    if (isPortNotAvailable()) {
+      throw new NullPointerException();
+    }
+    obtainGroup.create(group);
+  }
 
-  public void authorizeCardToDisplayInGroup(final String groupName, final String cardName) {}
+  public void authorizeCardToDisplayInGroup(final String groupName, final String cardName) {
+    if (isPortNotAvailable()) {
+      throw new NullPointerException();
+    }
+    obtainGroup.authorizeCardToDisplayInGroup(groupName, cardName);
+  }
 
-  public void approveCardChangesInTheGroup(final String groupName, final String cardName) {}
+  public void approveCardChangesInTheGroup(final String groupName, final String cardName) {
+    if (isPortNotAvailable()) {
+      throw new NullPointerException();
+    }
+    obtainGroup.approveCardChangesInTheGroup(groupName, cardName);
+  }
+
+  private Boolean isPortNotAvailable() {
+    return isNull(obtainGroup);
+  }
 }
