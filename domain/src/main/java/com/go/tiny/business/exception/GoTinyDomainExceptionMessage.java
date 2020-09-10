df2a3c71df4f73e05fa852630730164cd5b708b1
ganesh0479 <ganesh0479@gmail.com>
2020-09-10 13:50:54 +0530

    (domain-exception): updated class signature for exception message class

--- domain/src/main/java/com/go/tiny/business/exception/GoTinyDomainExceptionMessage.java
+++ domain/src/main/java/com/go/tiny/business/exception/GoTinyDomainExceptionMessage.java
@@ -1,12 +1,8 @@
 package com.go.tiny.business.exception;
 
-public class GoTinyDomainExceptionMessage {
-  private GoTinyDomainExceptionMessage() {}
-
-  public static final String USER_RIGHT_SIDE_PORT_UNAVAILABLE =
-      "User domain right side port is not available";
-  public static final String CARD_GROUP_RIGHT_SIDE_PORT_UNAVAILABLE =
+public interface GoTinyDomainExceptionMessage {
+  String USER_RIGHT_SIDE_PORT_UNAVAILABLE = "User domain right side port is not available";
+  String CARD_GROUP_RIGHT_SIDE_PORT_UNAVAILABLE =
       "Card group domain right side port is not available";
-  public static final String GROUP_RIGHT_SIDE_PORT_UNAVAILABLE =
-      "Group domain right side port is not available";
+  String GROUP_RIGHT_SIDE_PORT_UNAVAILABLE = "Group domain right side port is not available";
 }
