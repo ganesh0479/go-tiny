602ac2854e2dc22619a355626159a7e0a6cc2b43
ganesh0479 <ganesh0479@gmail.com>
2020-09-11 12:17:03 +0530

    fix(codacy): added missed issues fix of unnecessary imports

--- domain/src/main/java/com/go/tiny/business/model/Card.java
+++ domain/src/main/java/com/go/tiny/business/model/Card.java
@@ -1,6 +1,10 @@
 package com.go.tiny.business.model;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Builder
 @Getter
